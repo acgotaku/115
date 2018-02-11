6f6c3b49b82f4a43357abbb9fa1ca7faf16d1c6d
acgotaku <acgotaku311@gmail.com>
2018-02-11 23:42:23 +0900

    update version

--- src/js/lib/ui.js
+++ src/js/lib/ui.js
@@ -3,8 +3,8 @@ import Store from './store'
 
 class UI {
   constructor () {
-    this.version = '0.2.6'
-    this.updateDate = '2018/01/31'
+    this.version = '0.2.7'
+    this.updateDate = '2018/02/11'
     this.context = document.querySelector('iframe[rel="wangpan"]').contentDocument
     Store.on('updateView', (configData) => {
       this.updateSetting(configData)
