ce4cf5488916c9a8038990abd634ac15fd31f6c8
acgotaku <acgotaku311@gmail.com>
2020-12-16 19:45:59 +0900

    update version

--- src/js/lib/ui.js
+++ src/js/lib/ui.js
@@ -3,8 +3,8 @@ import Store from './store'
 
 class UI {
   constructor () {
-    this.version = '0.3.7'
-    this.updateDate = '2020/12/13'
+    this.version = '0.3.8'
+    this.updateDate = '2020/12/16'
     Store.on('updateView', (configData) => {
       this.updateSetting(configData)
       this.updateMenu(configData)
